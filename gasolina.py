425c76f74ab1064126cb219030dd62a821ec9dd3
BrunoFavaroSimoes <bruno.favaro.simoes@gmail.com>
2023-04-03 21:00:41 +0000

    arquivos gasolina.csv (arquivo), gasolina.png (gráfico), gasolina.py (código python) criados no branch develop

--- gasolina.py
+++ gasolina.py
@@ -1,6 +1,13 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt 
+
 df = pd.read_csv('gasolina.csv')
-sns.lineplot(data = df, x = 'dia', y = 'venda', color='g')
+grafico = sns.lineplot(data = df, x = 'dia', y = 'venda', color='b')
 plt.title('Valor da gasolina por dia')
 plt.xticks()
+plt.xlabel('Dia')
+plt.ylabel('Valor')
+plt.legend(['Valor da gasolina'])
 plt.savefig('gasolina.png', format='png')
 plt.show()
